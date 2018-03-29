1fd81d596b4fc13a0d060a6a0fa3ffc4fcd505b3
Ender Smith <Ender Smith>
2018-03-28 17:13:59 -0700

    update

--- wbc36/lib/binary-tree-compare.js
+++ wbc36/lib/binary-tree-compare.js
@@ -1,14 +1,12 @@
 'use strict';
 
-const BinaryTree = require('../model/bi-tree');
-
 const binaryTreeCompare = function(treeA, treeB) {
   if (!treeA || !treeB) throw new Error(`Error: Invalid input: ${treeA}, ${treeB}`);
   if (!treeA.hasOwnProperty('root') || (treeA.hasOwnProperty('left') && treeA.hasOwnProperty('right'))) throw new Error(`Error: Invalid input: ${treeA}, ${treeB}`);
   if (!treeB.hasOwnProperty('root') || (treeB.hasOwnProperty('left') && treeB.hasOwnProperty('right'))) throw new Error(`Error: Invalid input: ${treeA}, ${treeB}`);
 
   if(!treeA.root || !treeB.root) return (treeA.toString() === treeB.toString());
-  return (treeA.inOrderTraversal_checkStructure().toString() === treeB.inOrderTraversal_checkStructure().toString())
+  return (treeA.inOrderTraversal_checkStructure().toString() === treeB.inOrderTraversal_checkStructure().toString());
 };
 
 module.exports = binaryTreeCompare;
